54b5b53ba6a56bcf07827c38b88e2d9c2a050648
Diego Nunes Santos <130621994+DdiegoSantos@users.noreply.github.com>
2023-09-01 20:19:02 -0300

    Update perfil.js

--- perfil.js
+++ perfil.js
@@ -1,4 +1,5 @@
-usm = document.querySelector("#sobremim")
+sm = document.querySelector("#sobremim")
+dados =document.getElementClassAll("dados")
 
 function aparecer(){
     sm.style.width= "60%"
@@ -6,5 +7,6 @@ function aparecer(){
     sm.style.opacity="1"
     sm.style.margin= "50px auto"
     sm.style.transition = "4s"
+    dados.style.display ="block"
     
 }
