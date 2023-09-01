e224a91ec0d21131230b7ec329047fd52bb1550d
Diego Nunes Santos <130621994+DdiegoSantos@users.noreply.github.com>
2023-09-01 17:43:07 -0300

    Update perfil.js

--- perfil.js
+++ perfil.js
@@ -2,7 +2,7 @@ sm = document.querySelector("#sobremim")
 
 function aparecer(){
     sm.style.width= "60%"
-    sm.style.minWidth="380px"
+    sm.style.minWidth="280px"
     sm.style.opacity="1"
     sm.style.margin= "50px auto"
     sm.style.transition = "4s"
