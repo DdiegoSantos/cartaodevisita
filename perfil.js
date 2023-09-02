d423039dc0b2ddea1b1e260dc0bc5dd7b8f90c63
Diego Nunes Santos <130621994+DdiegoSantos@users.noreply.github.com>
2023-09-02 15:17:43 -0300

    Update perfil.js

--- perfil.js
+++ perfil.js
@@ -5,36 +5,35 @@ dadosp =document.querySelector("#dadosp")
 dadosp2 =document.querySelector("#dadosp2")
 btoSM = document.querySelector("#botSM")
 body = document.body
+cabecalho = querySelector("#cabecalho")
 
 
 function aparecer(){
    	sm.style.width= "60%"
     sm.style.minWidth="280px"
-	sm.style.opacity="1"
-   	sm.style.margin= "auto"
+	sm.style.display="block"
+   	sm.style.margin= "0 auto 10px auto"
    	sm.style.transition = "1.2s"
-	sm.style.position="absolute"
-	sm.style.right="20%"
-	sm.style.left="20%"
+	
+	
+	cabecalho.style.display="none"
 	
 	sm.style.height = "auto"
-	body.style.height = "1024px"
+	body.style.height = "auto"
 
-   	   
-}
-function botSM(){
 	dadosh1.style.display ="block"
 	dadosh2.style.display ="block"
 	dadosp.style.display ="block"
 	dadosp2.style.display ="block"
-	
-	botSM.style.height="auto"
-	botSM.style.transition="5s"
-} 
+	botSM.style.display="none"  
+}
+
 function fechar(){
     dadosh1.style.display ="none"
 	dadosh2.style.display ="none"
 	dadosp.style.display ="none"
 	dadosp2.style.display ="none"
-	sm.style.opacity="0"
+	sm.style.display="none"
+	cabecalho.style.display="block"
+	botSM.style.display="block"
 }
